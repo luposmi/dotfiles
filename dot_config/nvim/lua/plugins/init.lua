da9daa2e8a26d15b5110ca4eef220fbf8df87c98
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2024-03-27 11:03:24 +0100

    remove flutter

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -9,12 +9,4 @@ return {
         -- your configuration comes here
         -- or leave it empty to use the default settings
     }
-},{
-    'akinsho/flutter-tools.nvim',
-    lazy = false,
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'stevearc/dressing.nvim', -- optional for vim.ui.select
-    },
-    config = true,
 }
