b21e3be1f2907d38d0af7f093e9ec30d0fcbc483
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2024-11-17 07:53:39 +0100

    various, add activity-watcher, update, fix tmux conf

--- dot_config/nvim/lua/plugins/lsp-zero.lua
+++ dot_config/nvim/lua/plugins/lsp-zero.lua
@@ -1,6 +1,6 @@
 return {
     "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
+    branch = "v4.x",
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
