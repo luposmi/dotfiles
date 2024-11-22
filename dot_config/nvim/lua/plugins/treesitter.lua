b0fa486d3144727615207df2273e8163f87ef57e
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2024-11-22 12:20:15 +0100

    fix treesitter context

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -30,9 +30,4 @@ return {
                 }
             }
         }
-}, {
-    "nvim-treesitter/nvim-treesitter-context",
-    config = function()
-        require("treesitter-context").setup()
-    end
-}
+    }
