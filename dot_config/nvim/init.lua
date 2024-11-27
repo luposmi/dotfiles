2879be5b67ee39166828647d5fd6e3b0fdb626e0
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2024-11-27 12:09:15 +0100

    add OSC 52 support to tmux/nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -23,3 +23,15 @@ vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
         vim.bo.filetype = "yaml"
     end,
 })
+
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
