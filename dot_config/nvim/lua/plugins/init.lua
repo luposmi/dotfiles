5693a8458399a87b3823c1435198fc6f174b07a4
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2024-12-03 20:44:14 +0100

    increase timeout further

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -3,7 +3,6 @@ return {
     event = "VeryLazy",
     init = function()
         vim.o.timeout = true
-        vim.o.timeoutlen = 100
     end,
     opts = {
         -- your configuration comes here
