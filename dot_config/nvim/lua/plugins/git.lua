4ed231f51b711e195c7d247bf81305800cfa29a7
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2024-12-04 10:04:53 +0100

    remove optional plugins

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -111,8 +111,6 @@ local neogit = {
         "sindrets/diffview.nvim",    -- optional - Diff integration
         -- Only one of these is needed.
         "nvim-telescope/telescope.nvim", -- optional
-        "ibhagwan/fzf-lua",          -- optional
-        "echasnovski/mini.pick",     -- optional
     },
     config = function()
 
