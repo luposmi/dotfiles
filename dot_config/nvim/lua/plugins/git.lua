d58b1a40f0cbefd6c94100d8a93a4d640511120c
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2024-12-04 09:55:21 +0100

    add shortucts to gitsigns

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -54,6 +54,50 @@ return {{
             relative = 'cursor',
             row = 0,
             col = 1
-            }
-        }
+            },
+        on_attach = function(bufnr)
+            local gitsigns = require('gitsigns')
+
+            local function map(mode, l, r, opts)
+              opts = opts or {}
+              opts.buffer = bufnr
+              vim.keymap.set(mode, l, r, opts)
+            end
+
+            -- Navigation
+            map('n', ']c', function()
+              if vim.wo.diff then
+                vim.cmd.normal({']c', bang = true})
+              else
+                gitsigns.nav_hunk('next')
+              end
+            end)
+
+            map('n', '[c', function()
+              if vim.wo.diff then
+                vim.cmd.normal({'[c', bang = true})
+              else
+                gitsigns.nav_hunk('prev')
+              end
+            end)
+
+            -- Actions
+            map('n', '<leader>hs', gitsigns.stage_hunk, { desc = "gitsigns stage hung"})
+            map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = "gitsigns unstage hunk"})
+            map('n', '<leader>hr', gitsigns.reset_hunk, { desc = "gitsigns reset hunk"})
+            map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "gitsigns stage" })
+            map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "gitsigns reset" })
+            map('n', '<leader>hS', gitsigns.stage_buffer, { desc = "gitsigns stage buffer" })
+            map('n', '<leader>hR', gitsigns.reset_buffer, { desc = "gitsigns reset buffer" })
+            map('n', '<leader>hp', gitsigns.preview_hunk, { desc = "gitsigns preview hunk" })
+            map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end, { desc  = "gitsigns blame full" })
+            map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = "gitsigns current line blame" })
+            map('n', '<leader>hd', gitsigns.diffthis, { desc = "gitsigns diff this" })
+            map('n', '<leader>hD', function() gitsigns.diffthis('~') end, { desc = "gitsigns diff this" })
+            map('n', '<leader>htd', gitsigns.toggle_deleted, { desc = "gitsigns toggle delete" })
+
+            -- Text object
+            map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+          end
+    }
 }}
