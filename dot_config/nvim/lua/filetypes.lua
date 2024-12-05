093a57467ff897aa62b934039b9f8b0ba4ab4372
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2024-12-05 22:14:51 +0100

    add autodetection for kdl filetype

--- dot_config/nvim/lua/filetypes.lua
+++ dot_config/nvim/lua/filetypes.lua
@@ -1,12 +1,19 @@
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = "*.core",
     callback = function()
         vim.bo.filetype = "yaml"
     end,
 })
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = "*zshrc",
     callback = function()
         vim.bo.filetype = "zsh"
     end,
+})
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*.kdl",
+    callback = function()
+        vim.bo.filetype = "kdl"
+        vim.cmd("TSBufEnable highlight");
+    end
 })
