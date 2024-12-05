5f03a485a6fc53ffaca63f108c43c1457fad46c5
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2024-12-05 22:25:18 +0100

    format

--- dot_config/nvim/lua/filetypes.lua
+++ dot_config/nvim/lua/filetypes.lua
@@ -1,18 +1,18 @@
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
     pattern = "*.core",
     callback = function()
         vim.bo.filetype = "yaml"
     end,
 })
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
     pattern = "*zshrc",
     callback = function()
         vim.bo.filetype = "zsh"
     end,
 })
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
     pattern = "*.kdl",
-    callback = function()
+    callback = function ()
         vim.bo.filetype = "kdl"
         vim.cmd("TSBufEnable highlight");
     end
