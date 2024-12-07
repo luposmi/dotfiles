632a2f6ceec8eab3ab582e3e122004c2ef966ddf
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2024-12-07 18:35:30 +0100

    add rendering for obsidian todo list

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -5,7 +5,28 @@ local renderer = {
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    opts = {
+        checkbox = {
+            enabled = true,
+            position = 'inline',
+            unchecked = {
+                icon = '󰄱 ',
+                highlight = 'RenderMarkdownUnchecked',
+                scope_highlight = nil,
+            },
+            checked = {
+                icon = '󰱒 ',
+                highlight = 'RenderMarkdownChecked',
+                scope_highlight = nil,
+            },
+            custom = {
+                todo = { raw = '[-]', rendered = '', highlight = 'RenderMarkdownTodo', scope_highlight = nil },
+                rightarrow = { raw = '[>]', rendered = '󰧛' , highlight = 'RenderMarkdownRightArrow', scope_highlight = nil },
+                tilde = { raw = '[~]', rendered = '󰰱', highlight = 'RenderMarkdownTilde', scope_highlight = nil },
+                important = { raw = '[!]', rendered = '', highlight = 'RenderMarkdownImportant', scope_highlight = nil },
+            },
+        },
+    }
 }
 local obsidian = {
     "epwalsh/obsidian.nvim",
