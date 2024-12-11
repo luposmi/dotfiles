9063d60bb97bce130152b3021950cd75e2f25dda
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2024-12-11 17:32:57 +0100

    format and fix dependency of oil.nvim

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -1,10 +1,8 @@
 return {
-  {
     "echasnovski/mini.nvim",
     config = function()
-      require("mini.icons").setup()
-      require("mini.ai").setup()
-      require("mini.surround").setup()
-    end,
-  },
+        require("mini.icons").setup()
+        require("mini.ai").setup()
+        require("mini.surround").setup()
+    end
 }
