372fe322fc0c43e28d16120642eb4bcb1169fe4e
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2025-01-10 08:20:58 +0100

    various small changes

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -10,5 +10,8 @@ return {
         vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
         vim.keymap.set('n', '<leader>pt', builtin.treesitter, {})
+        vim.keymap.set('n', '<leader>sm',
+            function() require('telescope.builtin').lsp_document_symbols({ symbols = { 'function', 'method' } }) end,
+            { desc = "list all methods in file" })
     end
 }
