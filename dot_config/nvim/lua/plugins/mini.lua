fe8d6d0d03e88d783cc355c781b84c696e1031c0
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2025-01-19 13:11:06 +0100

    switch to mini clue form which key

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -1,8 +1,72 @@
 return {
     "echasnovski/mini.nvim",
     config = function()
-        require("mini.icons").setup()
         require("mini.ai").setup()
+        require("mini.align").setup()
+        require("mini.icons").setup()
         require("mini.surround").setup()
+        local clue = require("mini.clue")
+        clue.setup({
+            triggers = {
+               -- Leader triggers
+                { mode = 'n', keys = '<Leader>' },
+                { mode = 'x', keys = '<Leader>' },
+
+                -- Built-in completion
+                { mode = 'i', keys = '<C-x>' },
+
+                -- `g`        key
+                { mode = 'n', keys = 'g' },
+                { mode = 'x', keys = 'g' },
+
+                -- Mar        ks
+                { mode = 'n', keys = "'" },
+                { mode = 'n', keys = '`' },
+                { mode = 'x', keys = "'" },
+                { mode = 'x', keys = '`' },
+
+                -- Registers
+                { mode = 'n', keys = '"' },
+                { mode = 'x', keys = '"' },
+                { mode = 'i', keys = '<C-r>' },
+                { mode = 'c', keys = '<C-r>' },
+
+                -- Window commands
+                { mode = 'n', keys = '<C-w>' },
+
+                -- `z`        key
+                { mode = 'n', keys = 'z' },
+                { mode = 'x', keys = 'z' },
+                -- `@`        key
+                { mode = 'n', keys = '@' },
+                { mode = 'x', keys = '@' },
+
+                -- movement
+                { mode = 'n', keys = '[' },
+                { mode = 'x', keys = '[' },
+                { mode = 'n', keys = ']' },
+                { mode = 'x', keys = ']' },
+
+                -- around inside
+                { mode = 'v', keys = 'a' },
+                { mode = 'v', keys = 'i' },
+                { mode = 'v', keys = 'a' },
+                { mode = 'v', keys = 'i' },
+
+            },
+
+            clues = {
+                -- Enhance this by adding descriptions for <Leader> mapping groups
+                clue.gen_clues.builtin_completion(),
+                clue.gen_clues.g(),
+                clue.gen_clues.marks(),
+                clue.gen_clues.registers(),
+                clue.gen_clues.windows(),
+                clue.gen_clues.z(),
+            },
+            window = {
+                delay = 0
+            }
+        })
     end
 }
