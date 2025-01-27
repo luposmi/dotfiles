4fe83c736919077826d427f07398dc3aa2903fab
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2025-01-27 09:00:29 +0100

    change setup file for changes

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -13,7 +13,18 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("lazy").setup( {
+
+    spec = {
+        {import = "plugins"},
+        {import = "plugins/editing"},
+        {import = "plugins/language-server"},
+        {import = "plugins/menus"},
+        {import = "plugins/specific"},
+        {import = "plugins/visual"},
+
+    }
+})
 
 vim.treesitter.language.register("bash", "zsh")
 
