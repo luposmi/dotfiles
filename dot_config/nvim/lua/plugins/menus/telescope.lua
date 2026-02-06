1a0d69a6df5bb983d0a40b60d0aca71e735691ab
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2026-02-06 18:05:35 +0100

    Add fzf native back in

--- dot_config/nvim/lua/plugins/menus/telescope.lua
+++ dot_config/nvim/lua/plugins/menus/telescope.lua
@@ -1,7 +1,20 @@
 return {
     'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
     config = function()
+        require('telescope').setup {
+            extensions = {
+                fzf = {
+                    fuzzy = true,     -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true, -- override the file sorter
+                    case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                }
+            }
+        }
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
@@ -16,5 +29,6 @@ return {
         vim.keymap.set('n', '<leader>vs',
             function() require('telescope.builtin').lsp_document_symbols({ symbols = { 'function', 'method' } }) end,
             { desc = "list all methods in file" })
+        require('telescope').load_extension('fzf')
     end
 }
