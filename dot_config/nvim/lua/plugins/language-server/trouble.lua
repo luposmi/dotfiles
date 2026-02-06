285dced25eabb205a4b6240e886030943dea445f
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2026-02-06 18:05:30 +0100

    fix to last

--- dot_config/nvim/lua/plugins/language-server/trouble.lua
+++ dot_config/nvim/lua/plugins/language-server/trouble.lua
@@ -2,24 +2,7 @@
 return {
     "folke/trouble.nvim",
     require = "nvim-telescope/telescope.nvim",
-    config = function()
-        local actions = require("telescope.actions")
-        local open_with_trouble = require("trouble.sources.telescope").open
-
-        -- Use this to add more results without clearing the trouble list
-        local add_to_trouble = require("trouble.sources.telescope").add
-
-        local telescope = require("telescope")
-
-        telescope.setup({
-            defaults = {
-                mappings = {
-                    i = { ["<c-T>"] = open_with_trouble },
-                    n = { ["<c-T>"] = open_with_trouble },
-                },
-            },
-        })
-    end,
+    opts = {},
     cmd = "Trouble",
     keys = {
         {
