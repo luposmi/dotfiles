3470ff5365ec7a6072d9ef6dded3c2ffb787c0c2
Ben Mattes Krusekamp <git@luposmi.com>
2026-03-25 15:12:08 +0100

    remove duplicate verilator

--- dot_config/nvim/lua/filetypes.lua
+++ dot_config/nvim/lua/filetypes.lua
@@ -36,12 +36,3 @@ vim.api.nvim_create_autocmd("FileType", {
     })
   end,
 })
--- for verilator
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-    pattern = "*.core",
-    callback = function()
-        vim.bo.filetype = "yaml"
-    end,
-})
-
-
