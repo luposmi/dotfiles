4cc9dc1d8d17b650fb46c4a604d9c28e5fca46d3
Ben Mattes Krusekamp <git@luposmi.com>
2026-06-10 17:39:25 +0200

    make undotree persistent

--- dot_config/nvim/lua/plugins/editing/undotree.lua
+++ dot_config/nvim/lua/plugins/editing/undotree.lua
@@ -1,6 +1,15 @@
 return {
     "mbbill/undotree",
     config = function()
+        local undo_dir = vim.fn.stdpath("data") .. "/undo"
+        if vim.fn.isdirectory(undo_dir) == 0 then
+            vim.fn.mkdir(undo_dir, "p")
+        end
+
+        vim.opt.undodir = undo_dir
+        vim.opt.undofile = true
+        vim.opt.undolevels = 10000
+        vim.opt.undoreload = 10000
         vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
     end
 }
