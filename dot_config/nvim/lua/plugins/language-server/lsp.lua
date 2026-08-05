8190efe23970ca2486be0345b5d2548c8a158753
Ben Mattes Krusekamp <git@luposmi.com>
2026-08-05 13:18:31 +0200

    Remove bashls from mason lspconfig
    The version found in mason is to old to be usefull

--- dot_config/nvim/lua/plugins/language-server/lsp.lua
+++ dot_config/nvim/lua/plugins/language-server/lsp.lua
@@ -45,7 +45,7 @@ return {
 
         require('mason').setup({})
         require('mason-lspconfig').setup({
-            ensure_installed = { "bashls", "clangd", "lua_ls",  "zls", "verible" },
+            ensure_installed = { "clangd", "lua_ls",  "zls", "verible" },
             handlers = {
                 -- Default handler for installed servers
                 function(server_name)
